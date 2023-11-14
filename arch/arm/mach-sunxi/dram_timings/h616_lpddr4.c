0c76bd7d3b3e7d5dda895f2078bddbc498c0e4b3
ShohninDmitriy <demian77neo@mail.ru>
2023-11-14 08:10:59 +0300

    fix

--- arch/arm/mach-sunxi/dram_timings/h616_lpddr4.c
+++ arch/arm/mach-sunxi/dram_timings/h616_lpddr4.c
@@ -18,38 +18,38 @@ void mctl_set_timing_params(struct dram_para *para)
 	struct sunxi_mctl_ctl_reg * const mctl_ctl =
 			(struct sunxi_mctl_ctl_reg *)SUNXI_DRAM_CTL0_BASE;
 
-	u8 tccd		= 4;			/* JEDEC: 4nCK */
-	u8 tfaw		= ns_to_t(40);		/* JEDEC: 40 ns w/ 1K pages */
+	u8 tccd		= 4;					/* JEDEC: 4nCK */
+	u8 tfaw		= ns_to_t(40);			/* JEDEC: 40 ns w/ 1K pages */
 	u8 trrd		= max(ns_to_t(10), 2);	/* JEDEC: max(10 ns, 2nCK) */
 	u8 trcd		= max(ns_to_t(18), 2);	/* JEDEC: 13.5 ns */
-	u8 trc		= ns_to_t(65);		/* JEDEC: 49.5 ns */
+	u8 trc		= ns_to_t(65);			/* JEDEC: 49.5 ns */
 	u8 txp		= max(ns_to_t(8), 2);	/* JEDEC: max(6 ns, 3nCK) */
 	u8 trtp		= max(ns_to_t(8), 4);	/* JEDEC: max(7.5 ns, 4nCK) */
-	u8 trp		= ns_to_t(21);		/* JEDEC: >= 13.75 ns */
-	u8 tras		= ns_to_t(42);		/* JEDEC >= 36 ns, <= 9*trefi */
+	u8 trp		= ns_to_t(21);			/* JEDEC: >= 13.75 ns */
+	u8 tras		= ns_to_t(42);			/* JEDEC >= 36 ns, <= 9*trefi */
 	u16 trefi	= ns_to_t(3904) / 32;	/* JEDEC: 7.8us@Tcase <= 85C */
-	u16 trfc	= ns_to_t(280);		/* JEDEC: 160 ns for 2Gb */
-	u16 txsr	= ns_to_t(190);		/* ? */
+	u16 trfc	= ns_to_t(280);			/* JEDEC: 160 ns for 2Gb */
+	u16 txsr	= ns_to_t(190);			/* ? */
 
 	u8 tmrw		= max(ns_to_t(14), 5);	/* ? */
-	u8 tmrd		= tmrw;			/* JEDEC: 4nCK */
-	u8 tmod		= 12;			/* JEDEC: max(15 ns, 12nCK) */
+	u8 tmrd		= tmrw;					/* JEDEC: 4nCK */
+	u8 tmod		= 12;					/* JEDEC: max(15 ns, 12nCK) */
 	u8 tcke		= max(ns_to_t(15), 2);	/* JEDEC: max(5.625 ns, 3nCK) */
 	u8 tcksrx	= max(ns_to_t(2), 2);	/* JEDEC: max(10 ns, 5nCK) */
 	u8 tcksre	= max(ns_to_t(5), 2);	/* JEDEC: max(10 ns, 5nCK) */
-	u8 tckesr	= tcke;			/* JEDEC: tCKE(min) + 1nCK */
-	u8 trasmax	= (trefi * 9) / 32;	/* JEDEC: tREFI * 9 */
-	u8 txs		= 4;			/* JEDEC: max(5nCK,tRFC+10ns) */
-	u8 txsdll	= 16;			/* JEDEC: 512 nCK */
-	u8 txsabort	= 4;			/* ? */
-	u8 txsfast	= 4;			/* ? */
-	u8 tcl		= 10;			/* JEDEC: CL / 2 => 6 */
-	u8 tcwl		= 5;			/* JEDEC: 8 */
-	u8 t_rdata_en	= 17;			/* ? */
+	u8 tckesr	= tcke;					/* JEDEC: tCKE(min) + 1nCK */
+	u8 trasmax	= (trefi * 9) / 32;		/* JEDEC: tREFI * 9 */
+	u8 txs		= 4;					/* JEDEC: max(5nCK,tRFC+10ns) */
+	u8 txsdll	= 16;					/* JEDEC: 512 nCK */
+	u8 txsabort	= 4;					/* ? */
+	u8 txsfast	= 4;					/* ? */
+	u8 tcl		= 10;					/* JEDEC: CL / 2 => 6 */
+	u8 tcwl		= 5;					/* JEDEC: 8 */
+	u8 t_rdata_en	= 17;				/* ? */
 	u8 tphy_wrlat	= 5;
 
-	u8 twtp		= 24;			/* (WL + BL / 2 + tWR) / 2 */
-	u8 twr2rd	= max(trrd, (u8)4) + 14;/* (WL + BL / 2 + tWTR) / 2 */
+	u8 twtp		= 24;							/* (WL + BL / 2 + tWR) / 2 */
+	u8 twr2rd	= max(trrd, (u8)4) + 14;		/* (WL + BL / 2 + tWTR) / 2 */
 	u8 trd2wr	= (ns_to_t(4) + 17) - ns_to_t(1);/* (RL + BL / 2 + 2 - WL) / 2 */
 
 	/* set DRAM timing */
